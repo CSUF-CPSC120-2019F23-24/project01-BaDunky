c1b44217db8a98d1368c2a222bc9803a690ba3cc
BaDunky <arturitosilva20@hotmail.com>
2019-09-07 14:55:45 -0700

    added inputs

--- main.cpp
+++ main.cpp
@@ -1,2 +1,29 @@
-// Name:
+// Name: Arturo Silva
 // This program calculates and displays business expenses.
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main(){
+  std::string location;
+  int days;
+  double hotel;
+  double meal;
+  double total;
+
+  std::cout << "Welcome to the Business Trip Tracker!\n\nWhat is the business trip location? ";
+  std::cin >> location;
+  std::cout << "How many days will the trip take? ";
+  std::cin >> days;
+  std::cout << "What is the total hotel expense? ";
+  std::cin >> hotel;
+  std::cout << "What is the total meal expense? " ;
+  std::cin >> meal;
+  std::cout << '/n';
+
+  std::cout << "Location" << std::setw(12) << "Days" << std::setw(12) << "Hotel" << std::setw(12) << "meal"
+
+
+
+
+}
